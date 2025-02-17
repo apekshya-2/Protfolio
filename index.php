a2c20e54cbc3a02f08c629638aaa4cd21ebb8101
apeksha <dhunganaapekshya3@gmail.com>
2025-02-17 18:28:24 +0545

    first

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apeksha Dhungana</title>
+    <!-- this is for google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Oswald:wght@300&family=Poppins&family=Rajdhani&family=Yanone+Kaffeesatz:wght@300&display=swap"
+        rel="stylesheet">
+    <!-- slick slider css link for slider -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <!-- fontAwesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- css file link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar flex item-center justify-between">
+
+            <div class="logo">
+                Protfo<span style="color: rgb(253, 249, 2);">lio</span>
+            </div>
+            <div class="nav-item">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#services">Services</a>
+                <a href="#contact">Contact</a>
+                <a href="#projects">Works</a>
+                <!-- <a href="#blog">Blogs</a> -->
+            </div>
+            <a href="#contact" class="btn btn-primary">Contract</a></a>
+            
+            </div>
+        </nav>
+    </header>
+    <!-- home section start -->
+    
+    <section class="home-section"id="home">
+        <div class="home flex item-center">
+            <div class="img flex1 flex justify-center">
+                <img src="image/apeksha.jpeg" alt="Apeksha">
+            </div>
+            <di class="home-text flex1">
+                <h4>Namaste</h4>
+                <h1>I'm Apeksha <span style="color: #3dcfd3;">Dhungana</span> </h1>
+                <p>Hi! I’m Apeksha Dhungana, a passionate content writer with a knack for crafting engaging and 
+                impactful content that brings ideas to life. I thrive on transforming concepts into compelling 
+                narratives using research, creativity, and strategic planning. Alongside working on diverse solo
+                projects, I’ve collaborated with talented teams, honing my skills in SEO writing, website content 
+                creation, scheduling, and project management. Currently, I’m exploring web technologies like PHP, 
+                JavaScript, and MySQL, expanding my expertise in content and technology.
+                </p>
+                
+        <a href="finalcv.pdf">
+            <button class="btn btn-primary">Download CV</button>
+        </a>
+                
+                
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="about"id="about">
+        <div class="container flex item-center">
+            <div class="row">
+                <div class="about-col-1">
+                    <img src="images/dhading.jpeg"alt="" aling="">
+                    </div>
+                    <div class="about-col-2">
+                        <h1 class="section-heading">About <span style="color: #3dcfd3;">Me</span></h1>
+                    
+
+                <h3> Hello</h3>
+                <p width="300px">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+                    possimus ullam est blanditiis nihil velit, molestiasfugit quidem veniam quisquam quis laboriosam,
+                    numquam voluptate. At minima consequuntur eius ducimus tempora? </p>
+                
+                <div class="social-links">
+                    <a href="https://www.facebook.com/dipika.dhungana.77"><img src="image/social-media/facebook-icon.png" alt=""></a>
+                    <a href="https://www.instagram.com/dipika.dhungana.77/"><img src="image/social-media/instagram-icon.png" alt=""></a>
+                    <a href="https://www.linkedin.com/in/apeksha-dhungana-81460a282/"><img src="image/social-media/linkedin-icon.png" alt=""></a>
+                    <a href="https://x.com/dh2836"><img src="image/social-media/twitter-icon.png" alt=""></a>
+                    <a href="#"><img src="image/social-media/whatsapp-icon.png" alt=""></a>
+                </div>
+            </div>
+            </div>
+            <div class="img-about  flex1">
+                <img src="image/baluddhar.jpeg" alt="" align="relative" width="300px" height="300px">
+                
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Services section start -->
+    <section class="services"id="services">
+        <div class="container">
+            <h1 class="section-heading"> <span style="color: #3dcfd3;">Services</span></h1>
+            <p>How can I help you?</p>
+            <div class="card-wraper">
+                <div class="card">
+                    <i class="fas fa-desktop"></i>
+                    <h3>Blog writting</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, vel ducimus. Officiis, ipsam laudantium. Assumenda
+                        accusamus, distinctio iusto quasi quod cumque tempora deleniti explicabo officia at. Sapiente iste earum ad.</p>
+                </div>
+                <div class="card">
+                    <i class="fas fa-laptop-code"></i>
+                    <h3>Research & Development</h3>
+                    <p>I help you develop a well researched website with an excellent user interface,ensuring you get the most out of it.</p>
+                </div>
+                <div class="card">
+                    <i class="fas fa-photo-video"></i>
+                    <h3>Branding & Marketing</h3>
+                    <p>Need help with branding and marketing to reach out to a larger audience?I can help you create a brand presence and marketing efforts.</p>
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- freelancer section start -->
+    <section class="freelancer" id="freelancer">
+        <div class="container">
+            <h1>I Am Available For Freelancer.</h1>
+            <p>I can help you bring your ideas to life. Let’s talk about what I can build and raise together.</p>
+            
+            <a href="#contact" class="btn btn-primary">Hire me</a>
+        </div>
+    </section>
+
+    <!-- reviews section start -->
+    <section class="reviews" id="reviews">
+        <div class="container">
+            <h1 class="section-heading"><span style="color: #3dcfd3;"></span> What My Clients Say</h1>
+            <p></p>
+            <div class="slider">
+                <div class="slide">
+                    <img src=" image/binda.jpg" alt="">
+                    <p>Your writing is exceptionally clear, making even complex topics easy to understand. The way you engage readers from the first sentence is impressive</p>
+                    <span>Binda Shrestha</span>
+                </div>
+                <div class="slide">
+                    <img src="image/bhundol.jpeg" alt="">
+                    <p>Your ability to adapt your writing style to different topics and audiences is commendable. Whether it’s technical content or creative storytelling, you excel in both</p>
+                    <span>Anisha Pandey</span>
+                </div>
+                <div class="slide">
+                    <img src="image/dhading.jpeg" alt="">
+                    <p>The depth of research you put into your articles is evident. You always manage to provide well-supported arguments and credible sources, which adds great value to your work</p>
+                    <span>Nisha Dhungana</span>
+                </div>
+                <div class="slide">
+                    <img src="image/baluddhar.jpeg" alt="">
+                    <p>One thing I really appreciate about your work is its consistency. Every piece you deliver maintains a high standard of quality, with well-structured arguments and flawless grammar</p>
+                    <span>Sugarika Dotel</span>
+                </div>
+            </div>
+            <div class="slider-dots"></div>
+        </div>
+    </section>
+
+    <!-- projects section start -->
+    <section class="projects" id="projects">
+        <div class="container">
+            <h1 class="section-heading"><span style="color: #3dcfd3;">Works</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+            <div class="card-wraper">
+                <div class="card">
+                    <div class="overly">
+                        <span>Category</span>
+                        <a href="#">Web Development</a>
+                    </div>
+                    <img src="image/ph-1.png" alt="">
+                </div>
+                <div class="card">
+                    <div class="overly">
+                        <span>Category</span>
+                        <a href="#">Web Development</a>
+                    </div>
+                    <img src="image/ph-2.png" alt="">
+                </div>
+                <div class="card">
+                    <div class="overly">
+                        <span>Category</span>
+                        <a href="#">Web Development</a>
+                    </div>
+                    <img src="image/ph-3.png" alt="">
+                </div>
+                <div class="card">
+                    <div class="overly">
+                        <span>Category</span>
+                        <a href="#">Web Development</a>
+                    </div>
+                    <img src="image/ph-3.png" alt="">
+                </div>
+                <div class="card">
+                    <div class="overly">
+                        <span>Category</span>
+                        <a href="#">Web Development</a>
+                    </div>
+                    <img src="image/ph-1.png" alt="">
+                </div>
+                <div class="card">
+                    <div class="overly">
+                        <span>Category</span>
+                        <a href="#">Web Development</a>
+                    </div>
+                    <img src="image/ph-2.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="blog"id="blog">
+        <div class="container">
+            <h1 class="section-heading"><span style="color: #3dcfd3;">Blog</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+            <div class="card-wraper">
+                <div class="card">
+                    <div class="img-wraper">
+                        <img src="image/" alt="">
+                    </div>
+                    <div class="card-content">
+                        <a href="#"><h1>Lorem ipsum dolor.</h1></a>
+                        <span>May 12, 2024</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, fugiat officia cupiditate laboriosam, impedit veritatis doloribus quo esse error unde neque dolore temporibus est! Laboriosam ea aliquam architecto cupiditate enim?</p>
+                        <a class="read-more" href="#">Read More</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img-wraper">
+                        <img src="image/article-ph-1.png" alt="">
+                    </div>
+                    <div class="card-content">
+                        
+                        <a href="#"><h1>Lorem ipsum dolor.</h1></a>
+                        <span>May 12, 2024</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, fugiat officia cupiditate laboriosam, impedit veritatis doloribus quo esse error unde neque dolore temporibus est! Laboriosam ea aliquam architecto cupiditate enim?</p>
+                        <a class="read-more" href="#">Read More</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img-wraper">
+                        <img src="image/article-ph-1.png" alt="">
+                    </div>
+                    <div class="card-content">
+                        <a href="#"><h1>Lorem ipsum dolor.</h1></a>
+                        <span>May 12, 2024</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, fugiat officia cupiditate laboriosam, impedit veritatis doloribus quo esse error unde neque dolore temporibus est! Laboriosam ea aliquam architecto cupiditate enim?</p>
+                        <a class="read-more" href="#">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="container">
+            <h1 class="section-heading"><span style="color: #3dcfd3;">Contact</span> Us</h1>
+            <p>We are here to help!</p>
+            <div class="card-wraper">
+                <div class="card">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h3>Call Us On</h3>
+                    <h6>9843520317</h6>
+                </div>
+                <div class="card">
+                    <i class="far fa-envelope"></i>
+                    <h3>Email Us At</h3>
+                    <h6>dhunganaapeksha3@gmail.com</h6>
+                </div>
+                
+            </div>
+            <form action="userinfo.php" method="post">
+                <div class="input-wrap">
+                    <input type="text" name="user" placeholder="Your name">
+                    <input type="email" name="email" placeholder="Your email">
+                </div>
+                <div class="input-wrap-2">
+                    <input type="text" name="subject" placeholder="Your subject....">
+                    <textarea name="message" id="" cols="30" rows="10"  placeholder="Your message...."></textarea>
+                </div>
+                <div class="btn">
+                    <button class="btn btn-primary">Send Message</button>
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <div class="social-link">
+            <a href="https://www.facebook.com/dipika.dhungana.77"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/dipika.dhungana.77/"><i class="fab fa-instagram"></i></a> 
+            <a href="https://www.linkedin.com/in/apeksha-dhungana-81460a282/"><i class="fab fa-linkedin-in"></i></a> 
+            <a href="https://x.com/dh2836"><i class="fab fa-twitter"></i></a> 
+            <a href=""><i class="fas fa-map-marker"></i></a>         
+        </div>
+        <p> copyright:2024|Develop by Apeksha Dhungana</p>
+    </footer>
+
+
+
+
+    <!-- this is slick slider js link for slider -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+    <!-- js file link -->
+    <script src="js/app.js"></script>
+</body>
+
+</html>
